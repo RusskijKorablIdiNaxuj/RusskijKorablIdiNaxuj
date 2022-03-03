fc79b6dcd20b61d3fffd2a73ad2518a67c04e98b
RusskijKorablIdiNaxuj <100557909+RusskijKorablIdiNaxuj@users.noreply.github.com>
2022-03-03 15:10:15 +0200

    Update flood_http.go

--- src/flood/flood_http.go
+++ src/flood/flood_http.go
@@ -20,14 +20,13 @@ func (t *Target) performHttp(ctx context.Context, addr string) error {
 	request.Header.Set("Host", t.host)
 	request.Header.Set("Connection", "Keep-Alive")
 	request.Header.Set("User-Agent", userAgent())
-	accept := acceptall[rand.Intn(len(acceptall))]
-	request.Header["User-Agent"] = []string{}
 	request.Header.Set("Pragma", "no-cache")
 	request.Header.Set("Cache-Control", "no-transform,no-store")
 	request.Header.Set("Keep-Alive", "timeout=1000")
 	request.Header.Set("Accept-Encoding", "gzip,deflate")
 	additionalHeaders(request)
 
+	accept := acceptall[rand.Intn(len(acceptall))]
 	for _, l := range strings.Split(accept, "\r\n") {
 		if l == "" {
 			continue
